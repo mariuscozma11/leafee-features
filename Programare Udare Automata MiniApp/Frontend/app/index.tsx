74437cc5640d7bc3a55a8e6492febcf9caea0e43
Marius Cozma <mariuscozmabe@gmail.com>
2025-05-22 15:06:28 +0300

    Final modal frontend

--- Programare Udare Automata MiniApp/Frontend/app/index.tsx
+++ Programare Udare Automata MiniApp/Frontend/app/index.tsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
+import { Pressable, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Program from "./program";
+import Programare from "./programare";
 
 export default function Index() {
+  const [openModal, setOpenModal] = useState(false) 
   return (
     <SafeAreaView>
-      <Program />
+      <Pressable
+        onPress={() => setOpenModal(true)}
+        style={{
+          backgroundColor: 'green',
+          padding: 10,
+          borderRadius: 5,
+          margin: 10
+        }}
+      >
+        <Text style={{ color: 'white', textAlign: 'center' }}>Open Program</Text>
+      </Pressable>
+      <Programare 
+      programModal={openModal}
+      closeProgramModal={()=>{
+        setOpenModal(!openModal)
+      }}
+      />
     </SafeAreaView>
   );
 }
